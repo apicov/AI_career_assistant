b93bd439edc9cbd77d020fe3a9305dacef92deb7
apicov <a.pico@posteo.de>
2025-07-20 02:06:15 +0200

    add agent.py with Agent class

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import os
+import json
+from tools import *
 
 load_dotenv(override=True)
-
 
 # Load the API keys from environment variables
 groq_api_key = os.getenv('GROQ_API_KEY')
@@ -22,12 +24,38 @@ class Agent:
 
     def get_response(self, user_input, history):
         input_messages =  [self.system_prompt] + history + [{"role": "user", "content": user_input}]
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=input_messages,
-            tools=self.tools_json,
-            tool_choice="required"
-        )
-        return response
+        output_messages = []
+        done = False
+        while not done:
+            try:
+                response = self.client.chat.completions.create(
+                    model=self.model,
+                    messages=input_messages,
+                    tools=self.tools_json,
+                    tool_choice="auto"
+                )
+                output_messages.append(response.choices[0].message)
 
-    
+                finish_reason = response.choices[0].finish_reason
+                if finish_reason == "tool_calls":
+                    tool_calls = response.choices[0].message.tool_calls
+                    results = self.handle_tool_calls(tool_calls)
+                    output_messages.extend(results)
+                else:
+                    done = True
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return []
+        return output_messages
+
+    def handle_tool_calls(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            print(f"Arguments: {arguments}", flush=True)
+            tool = self.tools[tool_name]
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
